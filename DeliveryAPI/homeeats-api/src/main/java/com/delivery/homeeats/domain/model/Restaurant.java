dc4a891d041e1d6e6466232dd5f311280d0718a2
Rafael Lorenzi <rafalopesdelorenzi@gmail.com>
2023-06-01 09:46:16 +0100

    aplication properties and database

--- DeliveryAPI/homeeats-api/src/main/java/com/delivery/homeeats/domain/model/Restaurant.java
+++ DeliveryAPI/homeeats-api/src/main/java/com/delivery/homeeats/domain/model/Restaurant.java
@@ -1,5 +1,70 @@
 package com.delivery.homeeats.domain.model;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Restaurant {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private BigDecimal deliveryFee;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getDeliveryFee() {
+		return deliveryFee;
+	}
+
+	public void setDeliveryFee(BigDecimal deliveryFee) {
+		this.deliveryFee = deliveryFee;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
 
 }
---
+++ DeliveryAPI/homeeats-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into kitchen (name) values ('Chinese');
+insert into kitchen (name) values ('Indian');
