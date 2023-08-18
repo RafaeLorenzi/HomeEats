3fc688d3a43921933b1b11e8a4a6d81c8c5a5c9e
Rafael Lorenzi <rafalopesdelorenzi@gmail.com>
2023-08-18 11:36:56 +0100

    extending the problem detail to include properties with violated constraints

--- DeliveryAPI/homeeats-api/src/main/java/com/delivery/homeeats/api/exceptionHandler/Problem.java
+++ DeliveryAPI/homeeats-api/src/main/java/com/delivery/homeeats/api/exceptionHandler/Problem.java
@@ -1,6 +1,7 @@
 package com.delivery.homeeats.api.exceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -22,5 +23,14 @@ public class Problem {
 	
 	private String userMessage;
 	private LocalDateTime timestamp;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		
+		private String name;
+		private String userMessage;
+	}
 
 }
