0adb59158f60c140c17e45b726c5945894a937e8
Rafael Lorenzi <rafalopesdelorenzi@gmail.com>
2023-09-22 10:47:16 +0100

    api test with flyway migration

--- DeliveryAPI/homeeats-api/src/test/java/com/delivery/homeeats/HomeeatsApiApplicationTests.java
+++ DeliveryAPI/homeeats-api/src/test/java/com/delivery/homeeats/HomeeatsApiApplicationTests.java
@@ -1,15 +1,22 @@
 package com.delivery.homeeats;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertFalse;
 
 import javax.validation.ConstraintViolationException;
 
+import org.apache.http.HttpStatus;
+import org.flywaydb.core.Flyway;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.sql.init.dependency.AbstractBeansOfTypeDatabaseInitializerDetector;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.delivery.homeeats.domain.exception.EntityInUseException;
@@ -17,50 +24,55 @@ import com.delivery.homeeats.domain.exception.KitchenNotFoundException;
 import com.delivery.homeeats.domain.model.Kitchen;
 import com.delivery.homeeats.domain.model.service.KitchenRegistrationService;
 
+import  io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-class RegisterKitchenIntegrationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
+class RegisterKitchenIT {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private Flyway flyway;
 	
 	@Autowired
 	private KitchenRegistrationService kitchenRegistrationService;
 	
+	@Before
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/kitchen";
+		
+		flyway.migrate();
+		
+	}
+	
+	
 	@Test
-	public void  shouldAtributeAId_WhenKitchenRegisterWithCorrectData () {
-		//Scenario
-		Kitchen newKitchen = new Kitchen();
-		newKitchen.setName("chinese");
-		
-		//Action
-		newKitchen = kitchenRegistrationService.addKitchen(newKitchen);
-		
-		//validation
-		assertThat(newKitchen).isNotNull();
-		assertThat(newKitchen.getId()).isNotNull();
-		
+	public void mostReturnStatus200_WhenConsultKitchen () {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(org.springframework.http.HttpStatus.OK.value());
 	}
 	
-	@org.junit.Test ( expected = ConstraintViolationException.class)
-	public void  shouldFail_WhenRegisterKitchenWithoutName() {
-		
-		//Scenario
-		Kitchen newKitchen = new Kitchen();
-		newKitchen.setName(null);
-		
-		//Action
-		newKitchen = kitchenRegistrationService.addKitchen(newKitchen);
-			
-		
-	}
 	
-	@org.junit.Test( expected = EntityInUseException.class)
-	public void shouldFail_WhenDeleteKitchenInUse() {
-		kitchenRegistrationService.remove(1L);
-	}
-	
-	@org.junit.Test(expected = KitchenNotFoundException.class)
-	public void mostFail_WhenDeleteKitchenNotFound() {
-		kitchenRegistrationService.remove(100L);
-		
+	@Test
+	public void mustReturnStatus201_WhenRegisterKitchen() {
+		given()
+			.body("{ \"name\": \"chinese\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(org.springframework.http.HttpStatus.CREATED.value());
 	}
 
 	
